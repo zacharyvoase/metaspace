2ae146fa04ab757eec868a7d86c6131635ab6d78
Zachary Voase <z@zacharyvoase.com>
2011-12-19 15:52:08 +0000

    Initial commit.

---
+++ metaspace.py
@@ -0,0 +1,37 @@
+"""
+Create Python modules using class syntax.
+
+Example:
+
+    # in mymodule.py:
+    class submodule(Namespace):
+        abc = 123
+
+    # somewhere else:
+    >>> from mymodule.submodule import abc
+    >>> abc
+    123
+"""
+
+import sys
+import types
+
+
+class NamespaceMeta(type):
+    def __new__(mcls, name, bases, attrs):
+        if attrs.get('__metaclass__') is NamespaceMeta:
+            return type.__new__(mcls, name, bases, attrs)
+        module = types.ModuleType(name)
+        if attrs['__module__'] == '!__main__':
+            attrs.pop('__module__')
+            module.__dict__.update(attrs)
+            sys.modules[name] = module
+        else:
+            module.__dict__.update(attrs)
+            sys.modules[attrs['__module__'] + '.' + name] = module
+        return module
+
+
+class Namespace(object):
+
+    __metaclass__ = NamespaceMeta
---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='metaspace',
+    version='0.0.1',
+    description='Create Python modules using class syntax.',
+    author='Zachary Voase',
+    author_email='z@zacharyvoase.com',
+    url='http://github.com/zacharyvoase/metaspace',
+    py_modules=['metaspace'],
+)
